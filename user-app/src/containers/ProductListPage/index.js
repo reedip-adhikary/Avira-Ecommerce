81702c06bb51258a47e2dbe396dad1d528415cbb
Reedip Adhikary <reedipadhikary@gmail.com>
2021-05-26 03:27:43 +0530

    changes

--- user-app/src/containers/ProductListPage/index.js
+++ user-app/src/containers/ProductListPage/index.js
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getProductsBySlug } from "../../actions";
 import Layout from "../../components/Layout";
-// import getParams from "../../utils/getParams";
-// import ClothingAndAccessories from "./ClothingAndAccessories";
-// import ProductPage from "./ProductPage";
-// import ProductStore from "./ProductStore";
-// import "./style.css";
+import { useDispatch, useSelector } from 'react-redux'
+import { generatePublicUrl } from '../../urlConfig'
+import "./style.css";
 
 /**
  * @author
@@ -12,26 +11,51 @@ import Layout from "../../components/Layout";
  **/
 
 const ProductListPage = (props) => {
-    //   const renderProduct = () => {
-    //     console.log(props);
-    //     const params = getParams(props.location.search);
-    //     let content = null;
-    //     switch (params.type) {
-    //       case "store":
-    //         content = <ProductStore {...props} />;
-    //         break;
-    //       case "page":
-    //         content = <ProductPage {...props} />;
-    //         break;
-    //       default:
-    //         content = <ClothingAndAccessories {...props} />;
-    //     }
 
-    //     return content;
-    //   };
+    const product = useSelector(state => state.product)
+    const dispatch = useDispatch();
+    useEffect(() => {
+        const { match } = props;
+        dispatch(getProductsBySlug(match.params.slug))
+    }, [])
 
-    // return <Layout>{renderProduct()}</Layout>;
-    return <Layout>Product List</Layout>;
+    return (
+        <Layout>
+            {
+                Object.keys(product.productsByPrice).map((key, index) => {
+                    return (
+                        <div className="card">
+                            <div className="cardHeader">
+                                <div>Under 10k</div>
+                                <button>View all</button>
+                            </div>
+                            <div className="underline"></div>
+                            <div style={{ display: 'flex' }}>
+                                {
+                                    product.productsByPrice[key].map(product =>
+                                        <div className="productContainer">
+                                            <div className="productImgContainer">
+                                                <img src={generatePublicUrl(product.productPictures[0].img)} alt="" />
+                                            </div>
+                                            <div className="productInfo">
+                                                <div>{product.name}</div>
+                                                <div>
+                                                    <span>4.3</span>
+                                                    <span>     549</span>
+                                                </div>
+                                                <div className="productPrice">{product.price}</div>
+                                            </div>
+                                        </div>)
+                                }
+
+                            </div>
+                        </div>
+                    )
+                })
+            }
+
+        </Layout>);
 };
 
 export default ProductListPage;
+
--- user-app/src/reducers/index.js
+++ user-app/src/reducers/index.js
@@ -1,9 +1,11 @@
 
 import categoryReducer from './category.reducer';
+import producrtReducer from './product.reducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
-    category: categoryReducer
+    category: categoryReducer,
+    producrt: producrtReducer
 });
 
 export default rootReducer;
